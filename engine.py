871b38f03c083004931d885bc83bb53e2eb08b13
kornienkosvitlana20 <kornienkosvitlana20@knu.ua>
2026-02-26 23:54:30 +0200

    feat: додано клас Paddle з логікою руху вліво та вправо
    Реалізовано ініціалізацію платформи та методи зміни координат з урахуванням меж екрану

--- engine.py
+++ engine.py
@@ -19,3 +19,22 @@ class Ball:
     def bounce_x(self):
         # Реверс швидкості по горизонталі при зіткненні
         self.speed_x *= -1
+
+class Paddle:
+    def __init__(self, x, y):
+        # Параметри ракетки: ширина, висота та початкові координати
+        self.width = 100
+        self.height = 15
+        self.x = x
+        self.y = y
+        self.speed = 10
+
+    def move_left(self):
+        # Рух вліво (обмеження x > 0 додасть напарниця в головному циклі або додамо пізніше)
+        if self.x > 0:
+            self.x -= self.speed
+
+    def move_right(self, screen_width):
+        # Рух вправо з урахуванням ширини вікна
+        if self.x < screen_width - self.width:
+            self.x += self.speed
