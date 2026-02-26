f6782bbd22eb64c53bf155569c62b523a3beb486
kornienkosvitlana20 <kornienkosvitlana20@knu.ua>
2026-02-26 23:51:38 +0200

    docs: додано коментарі до методів класу Ball
    Опис функціоналу для кращої читаємості коду

--- engine.py
+++ engine.py
@@ -1,5 +1,6 @@
 class Ball:
     def __init__(self, x, y):
+        # Початкові координати та фізичні параметри м'яча
         self.x = x
         self.y = y
         self.radius = 10
@@ -7,11 +8,14 @@ class Ball:
         self.speed_y = -5
 
     def move(self):
+        # Оновлення позиції м'яча на кожному кадрі гри
         self.x += self.speed_x
         self.y += self.speed_y
 
     def bounce_y(self):
+        # Реверс швидкості по вертикалі при зіткненні
         self.speed_y *= -1
 
     def bounce_x(self):
+        # Реверс швидкості по горизонталі при зіткненні
         self.speed_x *= -1
